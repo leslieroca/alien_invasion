1b255c9cf6df43dcb2fbfea50d1c402815686137
leslie <Leslie>
2020-06-05 01:25:37 -0700

    Adds a ship to the game.
    - Adds images/ship.bmp resource.
    - Draw a ship in the bottom-center of the game screen.

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -1,8 +1,7 @@
 import sys
-
 import pygame
-
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # Initialize pygame, settings and screen object.
@@ -11,6 +10,10 @@ def run_game():
     screen = pygame.display.set_mode(ai_settings.screen_mode)
     pygame.display.set_caption("Alien Invasion")
     clock = pygame.time.Clock()
+
+    # Make a ship
+    ship = Ship(screen)
+    
 
     # Start the main loop for the game.
     while True:
@@ -24,6 +27,7 @@ def run_game():
 
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
