2f3f08688873cebbac60f69b3cc4deecf7a32a5f
leslie <Leslie>
2020-06-08 19:56:48 -0700

    Moves ship to top and bottom

--- src/ship.py
+++ src/ship.py
@@ -18,23 +18,38 @@ class Ship:
         self.rect.bottom = self.screen_rect.bottom 
 
         # Store a decimal value for the ship's center.
-        self.center = float(self.rect.centerx)
+        self.center_x = float(self.rect.centerx)
+        self.center_y = float(self.rect.centery)
 
         # Movement flag
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """Update the ship's position based on the movement flag."""
+        
         # Update the ship's center value, not the rect.
-
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.center += self.ai_settings.ship_speed_factor
+            self.center_x += self.ai_settings.ship_speed_factor
+
         if self.moving_left and self.rect.left > 0: 
-            self.center -= self.ai_settings.ship_speed_factor
+            self.center_x -= self.ai_settings.ship_speed_factor
+
+        if self.moving_up and self.rect.top > 0:
+            self.center_y -= self.ai_settings.ship_speed_factor
+
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom: 
+            self.center_y += self.ai_settings.ship_speed_factor
+        
 
         # Update rect object from self.center.
-        self.rect.centerx = self.center
+        self.rect.centerx = self.center_x
+
+        self.rect.centery = self.center_y
+        
+
 
     def blitme(self):
         """Draw the ship at its current location."""
