b70a2df23dd9ac7e1159621f20ff2f72004bb810
leslie <Leslie>
2020-06-09 18:25:58 -0700

    Adds bullets to game.
    - Gives the ship the ability to shoot bullets to the aliens

--- src/game_functios.py
+++ src/game_functios.py
@@ -1,8 +1,9 @@
 import sys
 import pygame
+from bullet import Bullet
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Respond to keypresses."""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
@@ -12,6 +13,12 @@ def check_keydown_events(event, ship):
         ship.moving_up = True
     elif event.key == pygame.K_DOWN:
         ship.moving_down = True
+    elif event.key == pygame.K_SPACE:
+        # Create a new bullet and add it to the bullets group.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+
 
 def check_keyup_events(event, ship):
     """Respond to key releases."""
@@ -26,7 +33,7 @@ def check_keyup_events(event, ship):
 
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -34,16 +41,21 @@ def check_events(ship):
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)        
+            check_keydown_events(event, ai_settings, screen, ship, bullets)      
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen."""
 
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
+
+    # Redraw all bullets behind ship and aliens.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
     ship.blitme()
 
     # Make the most recently drawn screen visible.
